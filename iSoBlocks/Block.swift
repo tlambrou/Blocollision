1f1041d8ab2002ad54e071b79d86546ffd145c4d
Tassos Lambrou <lambrou@gmail.com>
2016-07-11 09:48:52 -0700

    Initial Commit

---
+++ iSoBlocks/Block.swift
@@ -0,0 +1,9 @@
+//
+//  Block.swift
+//  iSoBlocks
+//
+//  Created by Tassos Lambrou on 7/9/16.
+//  Copyright © 2016 Ssos Games. All rights reserved.
+//
+
+import Foundation
---
+++ iSoBlocks/Goal.swift
@@ -0,0 +1,9 @@
+//
+//  Goal.swift
+//  iSoBlocks
+//
+//  Created by Tassos Lambrou on 7/9/16.
+//  Copyright © 2016 Ssos Games. All rights reserved.
+//
+
+import Foundation
